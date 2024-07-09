66b07ba9dc5fdf980cb66a8748a246eb0402e233
gabrielaramburu <gabrielaramburu@gmail.com>
2024-07-09 18:58:44 -0300

    first commit

---
+++ 01_MvcEjemplo1/01_MvcEjemplo1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01_MvcEjemplo1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
