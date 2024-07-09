8e0c1ea36362006a7c14e4d202baee2f9092fc8d
gabrielaramburu <gabrielaramburu@gmail.com>
2024-07-09 19:01:34 -0300

    revert :eevert "first commit"
    This reverts commit 66b07ba9dc5fdf980cb66a8748a246eb0402e233.

--- 01_MvcEjemplo1/01_MvcEjemplo1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _01_MvcEjemplo1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
