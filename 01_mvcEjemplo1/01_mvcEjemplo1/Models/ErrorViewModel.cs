847c449b6e44ad6495d4d3e61c460c024f8c7692
gabrielaramburu <gabrielaramburu@gmail.com>
2024-07-10 18:37:40 -0300

    se borra primer ejemplo

--- 01_mvcEjemplo1/01_mvcEjemplo1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _01_mvcEjemplo1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
