a37ad956a2a4d4d605b7a4d573803b9e9a94cfd3
gabrielaramburu <gabrielaramburu@gmail.com>
2024-09-01 18:41:34 -0300

    nuevo ejemplo SignalR con Blazor

---
+++ 01_4b_BlazorServerSignalR_Grafica/01_4b_BlazorServerSignalR_Grafica/Hubs/SensorHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace _01_4b_BlazorServerSignalR_Grafica.Hubs
+{
+    public class SensorHub: Hub
+    {
+        public void NotificarCambioTemperatura(int temperatura)
+        {
+            Clients.All.SendAsync("CambioTemperatura", temperatura);
+        }
+    }
+}
---
+++ 01_4b_BlazorServerSignalR_Grafica/01_4b_BlazorServerSignalR_Grafica/Program.cs
@@ -0,0 +1,36 @@
+using _01_4b_BlazorServerSignalR_Grafica.Components;
+using _01_4b_BlazorServerSignalR_Grafica.Hubs;
+using Radzen;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddRadzenComponents();
+
+// agregado para utilizar SignalR
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapHub<SensorHub>("/cambioTemperatura");
+
+app.Run();
